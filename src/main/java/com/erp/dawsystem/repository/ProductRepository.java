46053fe68b35eaf4b44aa260a69108d089a3e18b
David Peña <d1806pena@gmail.com>
2025-07-20 17:34:01 -0500

    Realicé el ProductRepository implementando los metodos a utilizar.

--- src/main/java/com/erp/dawsystem/repository/ProductRepository.java
+++ src/main/java/com/erp/dawsystem/repository/ProductRepository.java
@@ -3,8 +3,18 @@ package com.erp.dawsystem.repository;
 import com.erp.dawsystem.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
-public class ProductRepository  {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    List<Product> findByCategory(String category);
+
+    List<Product> findByNameContainingIgnoreCase(String name);
+
+    List<Product> findByStockLessThanEqual(int stock);
+
+    List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
 }
+
