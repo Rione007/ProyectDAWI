a7a0f3dc6729cce2b5708c1654fb914c96b8e88d
Anderson Villegas <anderson020398.007@gmail.com>
2025-07-20 19:12:29 -0500

    actualizado seters y geters de producto

--- src/main/java/com/erp/dawsystem/entity/Product.java
+++ src/main/java/com/erp/dawsystem/entity/Product.java
@@ -25,4 +25,52 @@ public class Product {
 
     @Column(nullable = true)
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
