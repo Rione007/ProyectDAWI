51abbcba3815c56d798838bfe30337a9fa86a0e3
XioC21 <jaimexz2001@gmail.com>
2025-07-22 00:06:00 -0500

    Implemente clase User

---
+++ src/main/java/com/erp/dawsystem/entity/User.java
@@ -0,0 +1,55 @@
+package com.erp.dawsystem.entity;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Usuario") // Nombre de la tabla en la base de datos
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password; // Se espera que esté encriptada
+
+    @Column(nullable = false)
+    private String role; // ADMIN, VENDEDOR, etc.
+
+    // Sin relaciones por ahora
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
