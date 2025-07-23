9aa731fe086230b928a6635f98f85ef9988fdf74
Anderson Villegas <anderson020398.007@gmail.com>
2025-07-23 00:43:57 -0500

    Layout implementado

--- src/main/java/com/erp/dawsystem/DawsystemApplication.java
+++ src/main/java/com/erp/dawsystem/DawsystemApplication.java
@@ -3,11 +3,29 @@ package com.erp.dawsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
 @SpringBootApplication
 public class DawsystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DawsystemApplication.class, args);
+		//abrir el localhost
+		openHomePage();
+	}
+
+	private static void openHomePage() {
+		if (Desktop.isDesktopSupported()) {
+			try {
+				Desktop.getDesktop().browse(new URI("http://localhost:8080"));
+			} catch (IOException | RuntimeException | java.net.URISyntaxException e) {
+				System.err.println("No se pudo abrir el navegador: " + e.getMessage());
+			}
+		} else {
+			System.out.println("Desktop no es compatible en este sistema.");
+		}
 	}
 
 }
