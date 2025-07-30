86cb01f1f89ab4f0915208803ee3d0c275b17873
Leyton <julio_cancer_999@hotmail.com>
2025-07-29 20:37:34 -0500

    ya esta el username con el usuario que me e logeado en el nabvar

--- src/main/java/com/erp/dawsystem/config/GlobalControllerAdvice.java
+++ src/main/java/com/erp/dawsystem/config/GlobalControllerAdvice.java
@@ -15,6 +15,10 @@ public class GlobalControllerAdvice {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         if (auth != null && auth.isAuthenticated() && !"anonymousUser".equals(auth.getPrincipal())) {
+            // Obtener nombre de usuario
+            String username = auth.getName();
+
+            // Obtener rol y traducirlo
             String rol = auth.getAuthorities().stream()
                     .map(GrantedAuthority::getAuthority)
                     .findFirst()
@@ -26,6 +30,8 @@ public class GlobalControllerAdvice {
                 default -> "Invitado";
             };
 
+            // Agregar atributos al modelo
+            model.addAttribute("username", username);
             model.addAttribute("rolUsuario", rolTraducido);
         }
     }
