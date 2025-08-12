83c78505fb9b3ed62391fce32d24eb657209baea
Rione <anderson020398.007@gmail.com>
2025-08-11 21:00:56 -0500

    finalizado proyecto

--- src/main/java/com/erp/dawsystem/DawsystemApplication.java
+++ src/main/java/com/erp/dawsystem/DawsystemApplication.java
@@ -1,5 +1,6 @@
 package com.erp.dawsystem;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DawsystemApplication {
 
 	public static void main(String[] args) {
+
+//		//comentar de aqui
+//		Dotenv dotenv = Dotenv.load();
+//
+//		System.out.println("Conectando a DB en: " + dotenv.get("DB_URL"));
+//		//hasta aqui para usar mysql local
 		SpringApplication.run(DawsystemApplication.class, args);
 		System.out.println("Aplicación iniciada en http://localhost:9090/");
-
-
 	}
 }
